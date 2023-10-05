2ddc7ab2332f8c92e6704cf273b756b7307aad3b
ane4ka0205bf <amalanova@beyondfinance.com>
2023-10-04 22:39:57 -0500

    commit

--- trigger-job.sh
+++ trigger-job.sh
@@ -11,6 +11,6 @@ PAYLOAD="{\"event_type\":\"jenkins-triggered\",\"client_payload\":{\"pr_number\"
 # Use the curl command with the payload
 curl -X POST https://api.github.com/repos/ane4ka0205/terraform-lambda/dispatches \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  -H "Authorization: token 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" \
+  -H "Authorization: token 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" \
   -H "Accept: application/vnd.github+json" \
   -d "$PAYLOAD"
