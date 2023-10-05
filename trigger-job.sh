20b30e2ea1d4bd6b108ea14ec0bf8a47476f676a
ane4ka0205bf <amalanova@beyondfinance.com>
2023-10-04 22:42:35 -0500

    commit

--- trigger-job.sh
+++ trigger-job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define your variables
-CHANGE_ID="19"
+CHANGE_ID="21"
 
 
 # Use command substitution to insert the variables into the JSON payload
@@ -11,6 +11,6 @@ PAYLOAD="{\"event_type\":\"jenkins-triggered\",\"client_payload\":{\"pr_number\"
 # Use the curl command with the payload
 curl -X POST https://api.github.com/repos/ane4ka0205/terraform-lambda/dispatches \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  -H "Authorization: token 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" \
+  -H "Authorization: token token" \
   -H "Accept: application/vnd.github+json" \
   -d "$PAYLOAD"
